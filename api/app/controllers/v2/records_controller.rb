4cfbdf2eb59f6da07e7415dc99318f0c01f64ce3
Nic Aitch <nic@nicinabox.com>
2016-12-17 09:18:08 -0600

    Scaffold records, vehicles

--- api/app/controllers/v2/records_controller.rb
+++ api/app/controllers/v2/records_controller.rb
@@ -1,4 +1,22 @@
 module V2
   class RecordsController < ApplicationController
+    def index
+      vehicle = Vehicle.find(params[:vehicle_id])
+      records = vehicle.records.all
+      render json: records
+    end
+
+    def create
+      record = Record.create(params[:record])
+      render json: record
+    end
+
+    def update
+
+    end
+
+    def destroy
+      Record.destroy(params[:id])
+    end
   end
 end
--- api/app/controllers/v2/vehicles_controller.rb
+++ api/app/controllers/v2/vehicles_controller.rb
@@ -1,8 +1,26 @@
 module V2
   class VehiclesController < ApplicationController
     def index
-      @vehicles = @current_user.vehicles.all
-      render json: @vehicles
+      vehicles = @current_user.vehicles.all
+      render json: vehicles
+    end
+
+    def create
+      vehicle = @current_user.vehicles.build(params[:vehicle])
+
+      if vehicle.save
+        render json: vehicle
+      else
+        render json: vehicle.errors, status: 400
+      end
+    end
+
+    def update
+
+    end
+
+    def destroy
+      @current_user.vehicle.destroy(params[:id])
     end
   end
 end
