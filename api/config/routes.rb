4faa190e6bc394eb4624b8f02e63bcc8c16a4fa3
Nic Aitch <nic@nicinabox.com>
2016-12-17 18:02:47 -0600

    Don't use api constraints with unauthed routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,11 @@
 require 'constraints/api_constraint'
 
 Rails.application.routes.draw do
+  scope module: :v2 do
+    post 'sessions', to: 'sessions#create'
+    get 'sessions/:login_token', to: 'sessions#login'
+  end
+
   scope module: :v2, constraints: ApiConstraint.new(version: 2) do
     resources :reminders
     resources :records
@@ -8,8 +13,6 @@ Rails.application.routes.draw do
 
     get 'user', to: 'users#index'
 
-    post 'sessions', to: 'sessions#create'
-    get 'sessions/:login_token', to: 'sessions#login'
     delete 'sessions', to: 'sessions#destroy'
   end
 end
