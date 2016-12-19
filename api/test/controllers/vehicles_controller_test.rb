6c9ce39e61ceac3dc933b655e25e2cf6ce2af6de
Nic Aitch <nic@nicinabox.com>
2016-12-18 23:20:26 -0600

    Configure vehicles controller test

--- api/test/controllers/vehicles_controller_test.rb
+++ api/test/controllers/vehicles_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class VehiclesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    new_session
+  end
+
+  test "gets all vehicles" do
+    get vehicles_url, http_options(@session.auth_token)
+    assert_equal [], response_body
+  end
 end
--- api/test/test_helper.rb
+++ api/test/test_helper.rb
@@ -7,4 +7,22 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def http_options(auth_token)
+    {
+      headers: {
+        accept: "application/json; version=2",
+        authorization: "Token #{auth_token}"
+      }
+    }
+  end
+
+  def response_body
+    JSON.parse(@response.body)
+  end
+
+  def new_session
+    @user = User.create({ email: "user@test" })
+    @session = @user.sessions.build(auth_token: SecureRandom.urlsafe_base64(24))
+    @session.save!
+  end
 end
