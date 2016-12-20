05d2eb924c51606d3a99e175c959f0d88f6bf967
Nic Aitch <nic@nicinabox.com>
2016-12-20 10:07:14 -0600

    Test reminders

--- api/test/controllers/reminders_controller_test.rb
+++ api/test/controllers/reminders_controller_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class RemindersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    new_session
+  end
+
+  test "returns all reminders for vehicle" do
+    vehicle = @user.vehicles.first
+
+    get vehicle_reminders_url(vehicle), http_options(@session.auth_token)
+    assert response_body.size == vehicle.reminders.count
+  end
+
+  test "update a reminder" do
+    vehicle = @user.vehicles.first
+    reminder = vehicle.reminders.first
+
+    put vehicle_reminder_url(vehicle, reminder), http_options(@session.auth_token).merge({
+      params: {
+        reminder: {
+          reminder: 'oil change'
+        }
+      }
+    })
+
+    assert response_body['reminder'] == 'oil change'
+  end
+
+  test "deletes all reminders for vehicle" do
+    vehicle = @user.vehicles.first
+
+    delete vehicle_reminder_url(vehicle.id, vehicle.reminders.last.id), http_options(@session.auth_token)
+    assert_response :success
+
+    get vehicle_reminders_url(vehicle), http_options(@session.auth_token)
+    assert response_body.size == 0
+  end
 end
