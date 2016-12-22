c06090f9dc36fa5fd584641e87be0a2e8e841700
Nic Aitch <nic@nicinabox.com>
2016-12-22 10:31:43 -0600

    Ship vehicle retired in api

--- api/app/serializers/vehicle_serializer.rb
+++ api/app/serializers/vehicle_serializer.rb
@@ -1,4 +1,4 @@
 class VehicleSerializer < ActiveModel::Serializer
-  attributes :id, :name, :vin, :notes, :position, :enable_cost, :created_at,
-    :miles_per_day, :miles_per_year
+  attributes :id, :name, :vin, :notes, :position, :enable_cost, :retired,
+    :created_at, :miles_per_day, :miles_per_year
 end
