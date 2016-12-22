38e1ab5796ba5429da126e76fc08abfde4dc626f
Nic Aitch <nic@nicinabox.com>
2016-12-22 12:01:32 -0600

    Add is_oil_change?

--- api/app/models/record.rb
+++ api/app/models/record.rb
@@ -2,4 +2,27 @@ class Record < ApplicationRecord
   validates_presence_of :notes, :date
 
   belongs_to :vehicle
+
+  def is_oil_change?
+    match_notes [
+      'engine oil',
+      'motor oil',
+      'change oil',
+      'changed oil',
+      'oil change',
+      'oil changed',
+    ]
+  end
+
+  private
+
+  def match_notes(tokens)
+    fragments = notes.split /,;\./
+
+    fragments.any? do |fragment|
+      tokens.any? do |token|
+        notes.match /#{token}/i
+      end
+    end
+  end
 end
