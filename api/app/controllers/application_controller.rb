40bbec17c4c705f26874cb11a93a365957b7e667
Nic Aitch <nic@nicinabox.com>
2016-12-23 11:33:34 -0600

    Logger logger output

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -6,18 +6,22 @@ class ApplicationController < ActionController::API
   before_action :authenticate
 
   rescue_from StandardError do |e|
+    logger.debug e
     respond_with_error(e.message, status: 500)
   end
 
   rescue_from ActiveRecord::RecordNotFound do |e|
+    logger.debug e
     respond_with_error(e.message, status: :not_found)
   end
 
   rescue_from ActiveRecord::RecordInvalid do |invalid|
+    logger.debug invalid
     respond_with_errors(invalid.record)
   end
 
   rescue_from ActionController::ParameterMissing do |e|
+    logger.debug e
     respond_with_error(e.message, status: :unprocessable_entity)
   end
 
