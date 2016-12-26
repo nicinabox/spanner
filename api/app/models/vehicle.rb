d4ea072e223ae28b2cc08819ae1188a6e57a0b0c
Nic Aitch <nic@nicinabox.com>
2016-12-26 15:21:02 -0600

    Simplify miles per x math

--- api/app/models/vehicle.rb
+++ api/app/models/vehicle.rb
@@ -11,26 +11,16 @@ class Vehicle < ApplicationRecord
     mpd = miles_per_day
     return if mpd.nil? or mpd < 0
 
-    first = records.order('date ASC').limit(1).first
-    elapsed_days = (Time.now - first.date).to_i / 1.day
-
-    if elapsed_days < ONE_YEAR
-      remaining_days = ONE_YEAR - Date.today.yday
-      mpy = mpd * (elapsed_days + remaining_days)
-    else
-      mpy = mpd * ONE_YEAR
-    end
-
-    (mpy / 10).floor * 10
+    (mpd * ONE_YEAR).to_i
   end
 
   def miles_per_day
-    first = records.first
-    last  = records.last
+    first = records.order('date ASC').first
+    last = records.order('date ASC').last
     return if last.nil? or last.mileage.nil?
 
     elapsed_days    = (last.date - first.date).to_i / 1.day
     elapsed_mileage = last.mileage - first.mileage
-    elapsed_mileage / elapsed_days
+    elapsed_mileage / elapsed_days.to_f
   end
 end
