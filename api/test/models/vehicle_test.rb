22c18664e6237eb7e4090d2e994c97579a0524ea
Nic Aitch <nic@nicinabox.com>
2016-12-26 23:41:23 -0600

    Fix failing model test

--- api/test/models/vehicle_test.rb
+++ api/test/models/vehicle_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 class VehicleTest < ActiveSupport::TestCase
   test "miles_per_day" do
     vehicle = Vehicle.first
-    assert_equal 99, vehicle.miles_per_day
+    assert_equal 99.97, vehicle.miles_per_day.round(2)
   end
 
   test "miles_per_year" do
     vehicle = Vehicle.first
-    assert_equal 4050, vehicle.miles_per_year
+    assert_equal 36487, vehicle.miles_per_year
   end
 end
