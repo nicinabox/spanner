a93c46894031c73a5cf8e0baba3dadbf09142b40
Nic Aitch <nic@nicinabox.com>
2016-12-28 13:13:11 -0600

    Add unique login subjects

--- api/app/mailers/login_mailer.rb
+++ api/app/mailers/login_mailer.rb
@@ -2,11 +2,15 @@ class LoginMailer < ApplicationMailer
 
   def login_link(user)
     @user = user
-    mail to: @user.email, subject: 'Sign in to Spanner'
+    mail to: @user.email, subject: "Sign in to Spanner (#{expires_time})"
   end
 
   def login_token(user)
     @user = user
-    mail to: @user.email, subject: 'Sign in to Spanner'
+    mail to: @user.email, subject: "Sign in to Spanner (#{expires_time})"
+  end
+
+  def expires_time
+    "expires #{(Time.now + 15.minutes).to_s(:short)}"
   end
 end
