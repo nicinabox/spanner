bc298bfd4c90114d83d5133fec7f5e0255f9e574
Nic Aitch <nic@nicinabox.com>
2017-01-07 12:54:32 -0600

    Don't load asset proxy in prod

--- server/index.js
+++ server/index.js
@@ -9,16 +9,9 @@ const ONE_YEAR = 31557600000
 
 const app = express()
 
-const assetProxy = proxy({
-  target: `http://localhost:${PORT - 1}`,
-  changeOrigin: true
-})
-
 const handler = (filename) => (req, res) => res.sendFile(path.join(__dirname, `../public/${filename}.html`))
 
 app.use('/', express.static(path.join(__dirname, '../public/'), { maxAge: ONE_YEAR }))
-app.use('/bundle.js*', assetProxy)
-app.use('/style.css*', assetProxy)
 app.use('/api', apiProxy)
 
 app.get('/apple-app-site-association', (req, res) => {
@@ -33,6 +26,14 @@ if (isProduction) {
   app.listen(PORT)
 } else {
   const webpack = require('./webpack')
+
+  const assetProxy = proxy({
+    target: `http://localhost:${PORT - 1}`,
+    changeOrigin: true
+  })
+  app.use('/bundle.js*', assetProxy)
+  app.use('/style.css*', assetProxy)
+
   webpack.listen(PORT - 1, 'localhost', () => {})
   app.listen(PORT)
 }
