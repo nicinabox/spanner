7a445125e4d1ae4d8b1075fcd951e7a70b33ed4e
Nic Aitch <nic@nicinabox.com>
2017-01-07 22:54:19 -0600

    Fix output public path

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, './public'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: isProduction ? '/' : 'http://localhost:8079',
   },
   module: {
     loaders: [
