d35525adc6d0024063ef7f310c1f5546dfe2206e
Nic Aitch <nic@nicinabox.com>
2017-04-04 01:39:51 -0500

    Autoload lib

--- api/config/application.rb
+++ api/config/application.rb
@@ -27,6 +27,8 @@ module SpannerApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.autoload_paths << Rails.root.join('lib')
+
     config.active_job.queue_adapter = :queue_classic
   end
 end
