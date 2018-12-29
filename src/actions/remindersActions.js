0ed52af284c5c832f068b7d839f30f34f73a0843
Nic Haynes <nic@nicinabox.com>
2018-12-29 11:18:07 -0600

    Add estimate date action

--- src/actions/remindersActions.js
+++ src/actions/remindersActions.js
@@ -4,6 +4,7 @@ export const RECEIVE_REMINDERS = 'RECEIVE_REMINDERS'
 export const CREATE_REMINDER = 'CREATE_REMINDER'
 export const DESTROY_REMINDER = 'DESTROY_REMINDER'
 export const UPDATE_REMINDER = 'UPDATE_REMINDER'
+export const ESTIMATE_REMINDER_DATE = 'ESTIMATE_REMINDER_DATE'
 
 export function fetchReminders(vehicleId) {
   return get(`/vehicles/${vehicleId}/reminders`, null, RECEIVE_REMINDERS, {
@@ -29,4 +30,13 @@ export function updateReminder(vehicleId, reminderId, params) {
     vehicleId,
     reminderId,
   })
+}
+
+export function estimateReminderDate(vehicleId, reminder) {
+  return get(
+    `/vehicles/${vehicleId}/reminders/estimate_date`,
+    { reminder },
+    ESTIMATE_REMINDER_DATE,
+    { vehicleId }
+  )
 }
