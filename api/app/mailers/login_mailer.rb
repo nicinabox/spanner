7ffb59de8adf0a43c2aa5626aa0bc670cbfafd37
Nic Haynes <nic@nicinabox.com>
2018-12-30 20:11:24 -0600

    Simplify login mailer subject

--- api/app/mailers/login_mailer.rb
+++ api/app/mailers/login_mailer.rb
@@ -2,16 +2,22 @@ class LoginMailer < ApplicationMailer
 
   def login_link(user)
     @user = user
-    mail to: @user.email, subject: "Sign in to Spanner (expires #{expires_time})"
+    mail to: @user.email, subject: subject
   end
 
   def login_token(user)
     @user = user
-    mail to: @user.email, subject: "Sign in to Spanner (expires #{expires_time})"
+    mail to: @user.email, subject: subject
+  end
+
+  private
+  def subject
+    "Sign in to Spanner (expires #{expires_time})"
   end
 
   def expires_time
-    time = Time.now
-    (time.in_time_zone(@user.time_zone) + 15.minutes).to_s(:short)
+    now = Time.now.in_time_zone @user.time_zone
+    time = now + 15.minutes
+    time.to_s(:short)
   end
 end
