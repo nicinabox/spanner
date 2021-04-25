70e2cc86774ee511b391bc265a062a9cc514010e
Nic Haynes <nic@nicinabox.com>
2021-04-25 15:34:25 -0500

    Use LinkOverlay

--- src/components/VehicleItem/index.tsx
+++ src/components/VehicleItem/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NextLink from 'next/link';
-import { Box, Button, Flex, Heading, HStack, Link, Text, useStyleConfig } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, HStack, Link, LinkBox, LinkOverlay, Text, useStyleConfig } from '@chakra-ui/react';
 import Interpunct from 'components/Interpunct';
 import { Vehicle } from 'queries/vehicles';
 import { formatEstimatedMileage, formatMilesPerYear } from 'utils/vehicle';
@@ -13,27 +13,29 @@ export const VehicleItem: React.FC<VehicleItemProps> = ({ vehicle}) => {
     const styles = useStyleConfig('VehicleItem');
 
     return (
-        <NextLink href={`/vehicles/${vehicle.id}`} passHref>
-            <Button as="a" sx={styles}>
-                <Flex direction="column" minH={12}>
-                    <Heading size="sm" color="brand.100">
-                        {vehicle.name}
-                    </Heading>
-                    <HStack divider={<Interpunct color="brand.300" fontSize="sm" />}>
-                        {Boolean(vehicle.estimatedMileage) && (
-                            <Text color="brand.300" fontSize="sm">
-                                ~{formatEstimatedMileage(vehicle)}
-                            </Text>
-                        )}
-                        {Boolean(vehicle.milesPerYear) && (
-                            <Text color="brand.300" fontSize="sm">
-                                {formatMilesPerYear(vehicle)}/yr
-                            </Text>
-                        )}
-                    </HStack>
-                </Flex>
-            </Button>
-        </NextLink>
+        <LinkBox sx={styles}>
+            <NextLink href={`/vehicles/${vehicle.id}`} passHref>
+                <LinkOverlay>
+                    <Flex direction="column" minH={12}>
+                        <Heading size="sm" color="brand.100">
+                            {vehicle.name}
+                        </Heading>
+                        <HStack divider={<Interpunct color="brand.300" fontSize="sm" />}>
+                            {Boolean(vehicle.estimatedMileage) && (
+                                <Text color="brand.300" fontSize="sm">
+                                    ~{formatEstimatedMileage(vehicle)}
+                                </Text>
+                            )}
+                            {Boolean(vehicle.milesPerYear) && (
+                                <Text color="brand.300" fontSize="sm">
+                                    {formatMilesPerYear(vehicle)}/yr
+                                </Text>
+                            )}
+                        </HStack>
+                    </Flex>
+                </LinkOverlay>
+            </NextLink>
+        </LinkBox>
     )
 };
 
