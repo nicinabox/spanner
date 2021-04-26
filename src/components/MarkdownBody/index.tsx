fc37bc84ef31efe32038043a49e26cd83c40addf
Nic Haynes <nic@nicinabox.com>
2021-04-26 00:34:02 -0500

    Yarn

--- src/components/MarkdownBody/index.tsx
+++ src/components/MarkdownBody/index.tsx
@@ -23,7 +23,7 @@ export const MarkdownBody: React.FC<MarkdownBodyProps> = ({ body }) => {
     return (
         <>
             <Head>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css" integrity="sha512-Oy18vBnbSJkXTndr2n6lDMO5NN31UljR8e/ICzVPrGpSud4Gkckb8yUpqhKuUNoE+o9gAb4O/rAxxw1ojyUVzg==" crossorigin="anonymous" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css" integrity="sha512-Oy18vBnbSJkXTndr2n6lDMO5NN31UljR8e/ICzVPrGpSud4Gkckb8yUpqhKuUNoE+o9gAb4O/rAxxw1ojyUVzg==" crossOrigin="anonymous" />
             </Head>
             <Box className="markdown-body" sx={styles} dangerouslySetInnerHTML={{ __html: marked(body) }} />
         </>
