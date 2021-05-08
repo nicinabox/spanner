09e96c1cb06ca66eedbf2214e4c405890305551c
Nic Haynes <nic@nicinabox.com>
2021-05-07 21:22:49 -0500

    Fix setting authorization

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,11 +1,11 @@
-import httpProxy from 'http-proxy'
+import httpProxy from 'http-proxy';
 import { Session } from 'queries/session';
 import { withSession } from 'utils/session';
 
-const PROXY_HOST = process.env.PROXY_HOST;
+const { PROXY_HOST } = process.env;
 
 if (!PROXY_HOST) {
-    throw new Error('PROXY_HOST not set')
+    throw new Error('PROXY_HOST not set');
 }
 
 // You can export a config variable from any API route in Next.js.
@@ -15,17 +15,17 @@ if (!PROXY_HOST) {
 // we can just stream all requests through to the actual API.
 export const config = {
     api: {
-        bodyParser: false
-    }
-}
+        bodyParser: false,
+    },
+};
 
 const proxy = httpProxy.createProxyServer();
 
 export default withSession((req, res) => {
     const session: Session | undefined = req.session.get('session');
 
-    proxy.once('proxyReq', function(proxyReq) {
-        proxyReq.setHeader('Accept', `application/vnd.api+json; version=2`);
+    proxy.once('proxyReq', (proxyReq) => {
+        proxyReq.setHeader('Accept', 'application/vnd.api+json; version=2');
 
         if (session) {
             proxyReq.setHeader('Authorization', `Token ${session.authToken}`);
@@ -35,14 +35,14 @@ export default withSession((req, res) => {
     // Return a Promise to let Next.js know when we're done processing the request
     return new Promise((resolve, reject) => {
         // Rewrite the URL: strip out the leading '/api'.
-        req.url = req.url.replace(/^\/api/, '')
+        req.url = req.url.replace(/^\/api/, '');
 
         // Don't forget to handle errors:
-        proxy.once('error', reject)
+        proxy.once('error', reject);
 
         // Forward the request to the API
         proxy.web(req, res, {
             target: PROXY_HOST,
-        })
-    })
+        });
+    });
 });
