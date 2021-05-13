aa7e35b6bf7749a96e8d8e6b8fb2eeeb4c2f7d20
Nic Haynes <nic@nicinabox.com>
2021-05-13 09:56:53 -0500

    Try updating api path

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,48 +1,38 @@
-import httpProxy from 'http-proxy';
-import { Session } from 'queries/session';
-import { withSession } from 'utils/session';
+import https from 'https';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const { PROXY_HOST } = process.env;
+// eslint-disable-next-line prefer-destructuring
+const PROXY_HOST = process.env.PROXY_HOST;
+// eslint-disable-next-line prefer-destructuring
+const NODE_ENV = process.env.NODE_ENV;
 
 if (!PROXY_HOST) {
     throw new Error('PROXY_HOST not set');
 }
 
-// You can export a config variable from any API route in Next.js.
-// We'll use this to disable the bodyParser, otherwise Next.js
-// would read and parse the entire request body before we
-// can forward the request to the API. By skipping the bodyParser,
-// we can just stream all requests through to the actual API.
+const isProd = NODE_ENV === 'production';
+
 export const config = {
     api: {
-        bodyParser: false,
+        // https://nextjs.org/docs/api-routes/api-middlewares#custom-config
+        externalResolver: true,
+        bodyParser: false, // not to use url encoded form like streaming POST request
     },
 };
 
-const proxy = httpProxy.createProxyServer();
+const host = new URL(PROXY_HOST).hostname;
 
-export default withSession((req, res) => {
-    const session: Session | undefined = req.session.get('session');
+export default createProxyMiddleware({
+    target: PROXY_HOST,
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+        const nextPath = path.replace('/api', '');
+        console.log(req.method, nextPath) // eslint-disable-line
 
-    proxy.once('proxyReq', (proxyReq) => {
-        proxyReq.setHeader('Accept', 'application/vnd.api+json; version=2');
-
-        if (session) {
-            proxyReq.setHeader('Authorization', `Token ${session.authToken}`);
-        }
-    });
-
-    // Return a Promise to let Next.js know when we're done processing the request
-    return new Promise((resolve, reject) => {
-        // Rewrite the URL: strip out the leading '/api'.
-        req.url = req.url.replace(/^\/api/, '');
-
-        // Don't forget to handle errors:
-        proxy.once('error', reject);
-
-        // Forward the request to the API
-        proxy.web(req, res, {
-            target: PROXY_HOST,
-        });
-    });
+        return nextPath;
+    },
+    agent: isProd ? https.globalAgent : false,
+    headers: {
+        host,
+    },
 });
