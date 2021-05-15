e09d09b02f2b521c49e2665c59ae7faf4a0f281b
Nic Haynes <nic@nicinabox.com>
2021-05-15 00:52:52 -0500

    Restore href to backbutton

--- pages/vehicles/[vehicleId].tsx
+++ pages/vehicles/[vehicleId].tsx
@@ -23,7 +23,7 @@ const PageHeader = ({ vehicle }) => (
         tabs={['Service', 'Notes']}
         LeftComponent={(
             <HStack spacing={2}>
-                <BackButton>
+                <BackButton href="/vehicles">
                     Vehicles
                 </BackButton>
 
--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -1,27 +1,42 @@
 import { ArrowBackIcon } from '@chakra-ui/icons';
+import Link from 'next/link';
 import { Button } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useCallback } from 'react';
 
 export interface BackButtonProps {
+    href?: string;
 }
 
-export const BackButton: React.FC<BackButtonProps> = ({ children }) => {
+export const BackButton: React.FC<BackButtonProps> = ({ href, children }) => {
     const router = useRouter();
 
+    const Container = useCallback(
+        (props) => {
+            if (href) return <Link href={href} passHref {...props} />;
+            return props.children;
+        },
+        [href],
+    );
+
     return (
-        <Button
-            onClick={(e) => {
-                e.preventDefault();
-                router.back();
-            }}
-            leftIcon={<ArrowBackIcon />}
-            size="sm"
-            variant="solid"
-            colorScheme="gray"
-        >
-            {children}
-        </Button>
+        <Container>
+            <Button
+                as={href ? 'a' : undefined}
+                onClick={(e) => {
+                    if (!href) {
+                        e.preventDefault();
+                        router.back();
+                    }
+                }}
+                leftIcon={<ArrowBackIcon />}
+                size="sm"
+                variant="solid"
+                colorScheme="gray"
+            >
+                {children}
+            </Button>
+        </Container>
     );
 };
 
