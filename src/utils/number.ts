bf72ec603e5542401b03a742fda3d5c2951d8aee
Nic Haynes <nic@nicinabox.com>
2021-05-15 00:14:59 -0500

    Update currency utils

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,7 +1,18 @@
+// TODO handle locale currency
+
 export function formatNumber(number: number, locale = 'en-US') {
     return new Intl.NumberFormat(locale, { maximumSignificantDigits: 6 }).format(number);
 }
 
 export function formatCurrency(number: number, locale = 'en-US') {
-    return new Intl.NumberFormat(locale, { style: 'currency', currency: 'USD' }).format(number);
+    return new Intl.NumberFormat(locale, { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(number);
+}
+
+export function getCurrencySymbol(currency = 'USD', locale = 'en-US') {
+    return (0).toLocaleString(locale, {
+        style: 'currency',
+        currency,
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+    }).replace(/\d/g, '').trim();
 }
