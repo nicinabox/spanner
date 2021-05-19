20a85660c84670978590434ef11184b239ea0e24
Nic Haynes <nic@nicinabox.com>
2021-05-18 22:45:58 -0500

    Fix story

--- src/components/common/LinkButton/LinkButton.stories.tsx
+++ src/components/common/LinkButton/LinkButton.stories.tsx
@@ -8,7 +8,8 @@ const Template: React.VFC<LinkButtonProps> = (props) => <LinkButton {...props} /
 
 export const Default = Template.bind({});
 Default.args = {
-
+    href: 'http://spanner.nicinabox.com',
+    children: 'Spanner',
 };
 
 export default {
