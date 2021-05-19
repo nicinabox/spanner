308739ca072331017d4fa82f1a32296f6c323c1c
Nic Haynes <nic@nicinabox.com>
2021-05-18 23:30:21 -0500

    Style reminders

--- src/components/VehicleReminders/VehicleReminders.tsx
+++ src/components/VehicleReminders/VehicleReminders.tsx
@@ -1,9 +1,11 @@
-import { AddIcon } from '@chakra-ui/icons';
+import { AddIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import {
-    Container, Flex, Heading, Text,
+    Container, Flex, Heading, LinkBox, LinkOverlay, StackDivider, Text, VStack,
 } from '@chakra-ui/react';
 import LinkButton from 'components/common/LinkButton';
+import useInlineColorMode from 'hooks/useInlineColorMode';
 import useRequest from 'hooks/useRequest';
+import Link from 'next/link';
 import { Vehicle, vehiclePath } from 'queries/vehicles';
 import React from 'react';
 import { intlFormatDateUTC } from 'utils/date';
@@ -16,6 +18,7 @@ export interface VehicleRemindersProps {
 
 export const VehicleReminders: React.FC<VehicleRemindersProps> = ({ vehicleId }) => {
     const { data: vehicle } = useRequest<Vehicle>(vehiclePath(vehicleId));
+    const cm = useInlineColorMode();
 
     return (
         <Container>
@@ -29,22 +32,39 @@ export const VehicleReminders: React.FC<VehicleRemindersProps> = ({ vehicleId })
 
             {vehicle?.reminders.map(((reminder) => {
                 return (
-                    <Flex key={reminder.id} py={2} borderBottomWidth={1} borderBottomColor="gray.100" minH={14}>
-                        <Flex flex={2} direction="column">
-                            <Text>
-                                {reminder.notes}
+                    <LinkBox
+                        key={reminder.id}
+                        py={3}
+                        px={4}
+                        mb={3}
+                        borderRadius={6}
+                        shadow="sm"
+                        bg={cm('white', 'whiteAlpha.200')}
+                        _hover={{ bg: cm('blackAlpha.50', 'whiteAlpha.300') }}
+                    >
+                        <Flex align="center">
+                            <Flex flex={2} direction="column">
+                                <Link href={`/vehicle/${vehicleId}/reminders/${reminder.id}`} passHref>
+                                    <LinkOverlay>
+                                        <Text>
+                                            {reminder.notes}
+                                        </Text>
+                                    </LinkOverlay>
+                                </Link>
+
+                                {reminder.mileage && (
+                                    <Text fontSize="sm" color={cm('blackAlpha.600', 'whiteAlpha.600')}>
+                                        {formatMileage(reminder.mileage, vehicle.distanceUnit)}
+                                    </Text>
+                                )}
+                            </Flex>
+                            <Text color={isReminderOverdue(reminder) ? cm('red.500', 'red.600') : 'whiteAlpha'} ml={3}>
+                                {reminder.reminderDate ? intlFormatDateUTC(reminder.reminderDate) : null}
                             </Text>
 
-                            {reminder.mileage && (
-                                <Text fontSize="sm">
-                                    {formatMileage(reminder.mileage, vehicle.distanceUnit)}
-                                </Text>
-                            )}
+                            <ChevronRightIcon w={5} h={5} ml={2} color={cm('blackAlpha.600', 'whiteAlpha.600')} />
                         </Flex>
-                        <Text color={isReminderOverdue(reminder) ? 'red.500' : 'black'}>
-                            {reminder.reminderDate ? intlFormatDateUTC(reminder.reminderDate) : null}
-                        </Text>
-                    </Flex>
+                    </LinkBox>
                 );
             }))}
 
