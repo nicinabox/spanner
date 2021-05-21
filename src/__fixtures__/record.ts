6071a2396608e2a372b7cfb62478784110747374
Nic Haynes <nic@nicinabox.com>
2021-05-20 21:41:32 -0500

    Fix fixture

--- src/__fixtures__/record.ts
+++ src/__fixtures__/record.ts
@@ -3,6 +3,8 @@ import { VehicleRecord } from 'queries/records';
 const recordFixure: VehicleRecord = {
     id: 2222,
     date: '2019-07-19T04:35:26.370Z',
+    createdAt: '2019-07-19T04:35:26.370Z',
+    updatedAt: '2019-07-19T04:35:26.370Z',
     cost: '',
     mileage: 0,
     notes: 'Record Fixture',
