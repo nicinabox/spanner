a94cdb1519092844168ca665f2ebb2adf4fac8ba
Nic Haynes <nic@nicinabox.com>
2021-05-31 17:12:40 -0500

    Update bugsnag to notify in prod

--- src/utils/bugsnag.ts
+++ src/utils/bugsnag.ts
@@ -4,4 +4,5 @@ import BugsnagPluginReact from '@bugsnag/plugin-react';
 Bugsnag.start({
     apiKey: process.env.BUGSNAG_API_KEY as string,
     plugins: [new BugsnagPluginReact()],
+    enabledReleaseStages: ['production'],
 });
