d154a7e5e54bcc98953949b282cf5f47c810ee8b
Nic Haynes <nic@nicinabox.com>
2021-11-21 17:46:27 -0600

    Handle sign in dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import {
-    Box, Center, Container, Flex,
+    Box, Center, Container, Flex, useColorModeValue,
 } from '@chakra-ui/react';
 import React from 'react';
 import { vehiclesPath } from 'utils/resources';
@@ -8,13 +8,15 @@ import LoginForm from 'components/forms/LoginForm';
 import Logo from 'components/Logo';
 
 export default function Home() {
+    const bg = useColorModeValue('white', 'brand.900');
+
     return (
-        <Flex h="100vh" bg="brand.primary" align="center" justify="center" direction="column">
-            <Center flex={1}>
+        <Flex minH="100vh" bg="brand.primary" align="center" justify="center" direction="column">
+            <Center flex={1} my={8}>
                 <Logo opacity={0.8} height={50} />
             </Center>
             <Container flex={3}>
-                <Box boxShadow="xl" bg="white" p={[2, 4, 8]} borderRadius="lg">
+                <Box boxShadow="xl" bg={bg} p={[2, 4, 8]} mb={8} borderRadius="lg">
                     <LoginForm />
                 </Box>
             </Container>
