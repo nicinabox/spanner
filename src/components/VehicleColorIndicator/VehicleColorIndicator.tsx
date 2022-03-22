cf32e18de555b0ad7899cf32f0d7da88ae6a7011
Nic Haynes <nic@nicinabox.com>
2022-03-21 20:08:37 -0500

    Keep vehicle color stable

--- src/components/VehicleColorIndicator/VehicleColorIndicator.tsx
+++ src/components/VehicleColorIndicator/VehicleColorIndicator.tsx
@@ -1,4 +1,4 @@
-import { Box, useColorModeValue } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 import getColorPalette from 'utils/colors';
 
@@ -9,11 +9,9 @@ export interface VehicleColorIndicatorProps {
 
 export const VehicleColorIndicator: React.FC<VehicleColorIndicatorProps> = ({ color, size = 6 }) => {
     const bg = color || '#fafafa';
-
     const p = getColorPalette(bg);
-    const secondaryBg = useColorModeValue(p[400], p[600]);
 
-    const background = `linear-gradient(0deg, ${secondaryBg} 50%, ${bg} 50%)`;
+    const background = `linear-gradient(0deg, ${p[600]} 50%, ${bg} 50%)`;
 
     return (
         <Box bg={background} w={size} h={size} />
