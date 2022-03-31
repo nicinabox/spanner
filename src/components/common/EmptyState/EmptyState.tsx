d213cdb19b352ba08fb7e3548b8819cd56ab4acc
Nic Haynes <nic@nicinabox.com>
2022-03-30 21:26:02 -0500

    Update empty state

--- src/components/common/EmptyState/EmptyState.tsx
+++ src/components/common/EmptyState/EmptyState.tsx
@@ -13,10 +13,10 @@ export const EmptyState: React.FC<EmptyStateProps> = ({ heading, details, action
     return (
         <Center>
             <Flex py={10} px={[null, 12]} my={8} flexDirection="column" align="center">
-                <Heading textAlign="center" size="lg">
+                <Heading textAlign="center" size="lg" mb={2}>
                     {heading}
                 </Heading>
-                <Text textAlign="center">{details}</Text>
+                <Text textAlign="center" fontSize="lg">{details}</Text>
 
                 {action && (
                     <Box mt={6}>
