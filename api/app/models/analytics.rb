9bb40fac8422a2bb75f6a4c239d9e69cad5e23d2
Nic Haynes <nic@nicinabox.com>
2025-09-24 20:59:59 -0500

    Fix analytics

--- api/app/models/analytics.rb
+++ api/app/models/analytics.rb
@@ -14,6 +14,7 @@ class Analytics
 
   def active_users
     User.joins(:sessions)
+        .select(:id, :email, :created_at, :updated_at)
         .select(user_fields)
         .where(sessions: {
                  last_seen: @start_date..@end_date
@@ -24,6 +25,7 @@ class Analytics
 
   def active_mobile_users
     User.joins(:sessions)
+        .select(:id, :email, :created_at, :updated_at)
         .where('sessions.description LIKE ?', '%Spanner%')
         .where(sessions: {
                  last_seen: @start_date..@end_date
@@ -34,6 +36,7 @@ class Analytics
 
   def total_mobile_users
     User.joins(:sessions)
+        .select(:id, :email, :created_at, :updated_at)
         .where('sessions.description LIKE ?', '%Spanner%')
         .distinct
         .order(created_at: :desc)
