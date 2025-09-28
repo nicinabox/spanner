f109d09dbfe0f2fc6413af3a268bf425ce95b89d
Nic Haynes <nic@nicinabox.com>
2025-09-28 14:33:12 -0500

    Fix build id

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
         return [
             now.getFullYear(),
             String(now.getMonth() + 1).padStart(2, '0'),
-            String(now.getDay()).padStart(2, '0'),
+            String(now.getDate()).padStart(2, '0'),
         ].join('');
     },
 
