12a1e9eacf761467e53b25089d4687606a3ff099
Nic Haynes <nic@nicinabox.com>
2025-09-28 22:39:47 -0500

    Add meta theme color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,25 @@ function App({ Component, pageProps }) {
         <ChakraProvider theme={theme}>
             <Head>
                 <title>Spanner</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="description" content="The easiest way keep service records for all your vehicles." />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <meta
+                    name="description"
+                    content="The easiest way keep service records for all your vehicles."
+                />
+                {}
+                <meta
+                    name="theme-color"
+                    content={theme.colors.brand.primary}
+                    media="(prefers-color-scheme: light)"
+                />
+                <meta
+                    name="theme-color"
+                    content={theme.colors.brand['600']}
+                    media="(prefers-color-scheme: dark)"
+                />
             </Head>
 
             <CSSReset />
