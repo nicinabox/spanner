c04b34bb2ce04a1cf1f09aedefd323c5789c878a
Nic Haynes <nic@nicinabox.com>
2025-09-28 19:01:23 -0500

    Adjust notes styles

--- src/components/VehicleNotes/VehicleNotes.tsx
+++ src/components/VehicleNotes/VehicleNotes.tsx
@@ -1,7 +1,5 @@
 import { EditIcon } from '@chakra-ui/icons';
-import {
-    Box, Button, Container, Flex,
-} from '@chakra-ui/react';
+import { Box, Button, Container, Flex } from '@chakra-ui/react';
 import EmptyState from 'components/common/EmptyState';
 import MarkdownBody from 'components/common/MarkdownBody';
 import NotesForm from 'components/forms/NotesForm';
@@ -23,37 +21,51 @@ export const VehicleNotes: React.FC<VehicleNotesProps> = ({ vehicle }) => {
             <EmptyState
                 heading="Notes are for hard-to-remember things"
                 details="Add tire pressures, oil capacity, or how to reset the clock."
-                action={(
-                    <Button colorScheme="brand" leftIcon={<EditIcon />} onClick={() => setEditing(true)} shadow="lg">
+                action={
+                    <Button
+                        colorScheme="brand"
+                        leftIcon={<EditIcon />}
+                        onClick={() => setEditing(true)}
+                        shadow="lg"
+                    >
                         Edit Notes
                     </Button>
-                )}
+                }
             />
         );
     }
 
     return (
-        <Container maxW="container.lg">
-            <Box shadow={['sm', 'md', 'md']} p={[4, 10]} mx={[-4, null]} bg={cm('white', 'whiteAlpha.200')}>
-                {editing ? (
-                    <NotesForm
-                        formValues={vehicle}
-                        onError={() => setEditing(true)}
-                        onSuccess={() => setEditing(false)}
-                        onCancel={() => setEditing(false)}
-                    />
+        <Container maxW="container.md">
+            {editing ? (
+                <NotesForm
+                    formValues={vehicle}
+                    onError={() => setEditing(true)}
+                    onSuccess={() => setEditing(false)}
+                    onCancel={() => setEditing(false)}
+                />
             ) : (
                 <>
                     <Flex direction="row-reverse" mb={6}>
-                        <Button colorScheme="brand" size="md" leftIcon={<EditIcon />} onClick={() => setEditing(true)}>
+                        <Button
+                            colorScheme="brand"
+                            size="md"
+                            leftIcon={<EditIcon />}
+                            onClick={() => setEditing(true)}
+                        >
                             Edit
                         </Button>
                     </Flex>
 
-                    <MarkdownBody body={vehicle.notes} />
+                    <Box
+                        shadow={['sm', 'md', 'md']}
+                        p={[4, 10]}
+                        bg={cm('white', 'whiteAlpha.200')}
+                    >
+                        <MarkdownBody body={vehicle.notes} />
+                    </Box>
                 </>
             )}
-            </Box>
         </Container>
     );
 };
