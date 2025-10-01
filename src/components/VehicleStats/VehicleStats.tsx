2856c3b85e13b92a337b542d152f3886a9742995
Nic Haynes <nic@nicinabox.com>
2025-09-30 21:15:31 -0500

    Hide webkit scrollbar on stats

--- src/components/VehicleStats/VehicleStats.tsx
+++ src/components/VehicleStats/VehicleStats.tsx
@@ -9,19 +9,40 @@ export interface VehicleStatsProps {
     records: API.Record[];
 }
 
-const ENABLED_STATS = ['EstimatedMileageStat', 'YearlyMileageStat', 'SinceStat', 'VinStat'];
+const ENABLED_STATS = [
+    'EstimatedMileageStat',
+    'YearlyMileageStat',
+    'SinceStat',
+    'VinStat',
+];
 
-export const VehicleStats: React.FC<VehicleStatsProps> = ({ vehicle, records }) => {
+export const VehicleStats: React.FC<VehicleStatsProps> = ({
+    vehicle,
+    records,
+}) => {
     const { isShared } = usePageContext();
     const [oldestRecord] = sortRecordsOldestFirst(records);
 
     return (
-        <Box overflowY="hidden" overflowX="auto" maxW="calc(100vw - 32px)" mb={6}>
+        <Box
+            overflowY="hidden"
+            overflowX="auto"
+            maxW="calc(100vw - 32px)"
+            mb={6}
+            sx={{
+                '&::-webkit-scrollbar': {
+                    display: 'none',
+                },
+            }}
+        >
             <HStack spacing={[8, null, 8]} pb={2}>
                 {ENABLED_STATS.map((name) => {
                     const VehicleStat = AllVehicleStats[name];
                     const props = {
-                        vehicle, records, oldestRecord, isShared,
+                        vehicle,
+                        records,
+                        oldestRecord,
+                        isShared,
                     };
                     return <VehicleStat key={name} {...props} />;
                 })}
