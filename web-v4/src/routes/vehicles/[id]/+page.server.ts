4b081bd788f3b6f8b641367d16135af441d2dc77
Nic Haynes <nic@nicinabox.com>
2025-11-08 13:05:05 -0600

    Scaffold vehicle history

--- web-v4/src/routes/vehicles/[id]/+page.server.ts
+++ web-v4/src/routes/vehicles/[id]/+page.server.ts
@@ -1,6 +1,11 @@
+import { getVehicleHistory } from '$lib/data/history';
 import { getVehicle } from '$lib/data/vehicles';
 
 export const load = async ({ locals, params }) => {
-	const vehicle = await getVehicle(params.id, locals);
-	return { vehicle };
+	const [vehicle, history] = await Promise.all([
+		getVehicle(params.id, locals),
+		getVehicleHistory(params.id, locals)
+	]);
+
+	return { vehicle, history };
 };
