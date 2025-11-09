e023c0511d16677d90e194fc341fdac1de7cd715
Nic Haynes <nic@nicinabox.com>
2025-11-09 16:30:36 -0600

    Extract flex table to component

---
+++ web-v4/src/lib/components/FlexTable/Table.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+<div class="flex w-full flex-col flex-nowrap py-2 sm:table">
+	{@render children()}
+</div>
