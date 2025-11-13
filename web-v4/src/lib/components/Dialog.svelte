d0587ea5758643f0c8b2f41a682885caf0287c8c
Nic Haynes <nic@nicinabox.com>
2025-11-12 21:31:36 -0600

    Add Dialog, vehicle delete

---
+++ web-v4/src/lib/components/Dialog.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		title: string;
+		children: Snippet;
+		actions: Snippet;
+		ref: HTMLDialogElement;
+	}
+
+	let { title, children, actions, ref = $bindable() }: Props = $props();
+</script>
+
+<dialog class="modal" bind:this={ref}>
+	<div class="modal-box">
+		<h3 class="mb-6 text-lg font-bold">{title}</h3>
+
+		<div class="my-4">
+			{@render children()}
+		</div>
+
+		{#if actions}
+			<div class="modal-action">
+				{@render actions()}
+			</div>
+		{/if}
+	</div>
+
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
+</dialog>
