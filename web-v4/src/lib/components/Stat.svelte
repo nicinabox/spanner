1960d419608d87310f5180f816b98d37f13d7425
Nic Haynes <nic@nicinabox.com>
2025-11-15 14:03:06 -0600

    Update Since stat

--- web-v4/src/lib/components/Stat.svelte
+++ web-v4/src/lib/components/Stat.svelte
@@ -7,7 +7,9 @@
 	let { title, value }: Props = $props();
 </script>
 
-<div class="stat border-0 p-0">
-	<div class="stat-title">{title}</div>
-	<div class="stat-value text-2xl font-medium">{value}</div>
-</div>
+{#if value}
+	<div class="stat border-0 p-0">
+		<div class="stat-title">{title}</div>
+		<div class="stat-value text-2xl font-medium">{value}</div>
+	</div>
+{/if}
--- web-v4/src/lib/utils/text.ts
+++ web-v4/src/lib/utils/text.ts
@@ -4,3 +4,7 @@ export function pluralize(count: number, singular: string, plural = `${singular}
 	const subject = pluralRules.select(count) === 'one' ? singular : plural;
 	return `${count} ${subject}`;
 }
+
+export function parameterize(text: string, sep = '-'): string {
+	return text.toLowerCase().replace(/[\W]+/g, sep);
+}
