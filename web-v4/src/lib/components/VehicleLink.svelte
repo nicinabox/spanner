4236451ae7f3538fd00a241a037f6ee32f2b9394
Nic Haynes <nic@nicinabox.com>
2025-11-16 19:30:02 -0600

    Update vehicle link with overdue reminders count

--- web-v4/src/lib/components/VehicleLink.svelte
+++ web-v4/src/lib/components/VehicleLink.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
 	import type { Vehicle } from '$lib/data/vehicles';
+	import { getOverdueRemindersCount } from '$lib/utils/reminders';
+	import { pluralize } from '$lib/utils/text';
 	import { formatMileage } from '$lib/utils/vehicle';
 
-	const { id, name, milesPerYear, estimatedMileage, distanceUnit }: Vehicle = $props();
+	const vehicle: Vehicle = $props();
+	const { id, name, milesPerYear, estimatedMileage, distanceUnit } = vehicle;
+
+	let overdueRemindersCount = getOverdueRemindersCount(vehicle);
 </script>
 
 <a
 	href={resolve(`/vehicles/${id}`)}
-	class="flex-1 rounded-md bg-base-200 p-4 shadow-sm hover:bg-primary"
+	class="min-h-[110px] flex-1 rounded-md bg-base-300 p-4 shadow-sm hover:bg-primary"
 >
 	<h2 class="font-medium">{name}</h2>
 
 	<span class="text-sm text-black/40 dark:text-white/50">
 		<span>{formatMileage(estimatedMileage, distanceUnit)}</span>
 		&bull;
-		<span>{formatMileage(milesPerYear, distanceUnit)}</span>
+		<span>{formatMileage(milesPerYear, distanceUnit)}/yr</span>
 	</span>
+
+	{#if overdueRemindersCount}
+		<span class="mt-2 badge block badge-sm badge-warning">
+			{pluralize(overdueRemindersCount, 'overdue reminder', 'overdue reminders')}
+		</span>
+	{/if}
 </a>
