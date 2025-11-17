f03068a84cfbab743f54e58133b3cfbeb3035150
Nic Haynes <nic@nicinabox.com>
2025-11-16 19:34:22 -0600

    Update Stat with class

--- web-v4/src/lib/components/Stat.svelte
+++ web-v4/src/lib/components/Stat.svelte
@@ -2,13 +2,14 @@
 	interface Props {
 		title: string;
 		value: string | number | null | undefined;
+		class?: string;
 	}
 
-	let { title, value }: Props = $props();
+	let { title, value, ...props }: Props = $props();
 </script>
 
 {#if value}
-	<div class="stat border-0 p-0">
+	<div class="stat border-0 p-0 {props.class}">
 		<div class="stat-title">{title}</div>
 		<div class="stat-value text-2xl font-medium">{value}</div>
 	</div>
