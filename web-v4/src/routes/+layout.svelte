59aab45d809755db06c73a8f2e98cfad9fd16d52
Nic Haynes <nic@nicinabox.com>
2026-03-22 14:35:01 -0500

    Update favicon

--- web-v4/src/routes/+layout.svelte
+++ web-v4/src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" href={favicon} />
 </svelte:head>
 
 {@render children()}
