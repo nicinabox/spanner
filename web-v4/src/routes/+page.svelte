04b2bc42b7e4f0286256e016f92cefdc9f87f84b
Nic Haynes <nic@nicinabox.com>
2026-03-22 14:17:29 -0500

    Add logos for light/dark mode

--- web-v4/src/routes/+page.svelte
+++ web-v4/src/routes/+page.svelte
@@ -6,10 +6,16 @@
 </script>
 
 <main class="container mx-auto">
-	<div class="flex h-screen flex-col items-center justify-center">
-		<div class="m-8 w-full rounded-xl border bg-card p-8 shadow-sm sm:w-[400px]">
-			<h1 class="h1">{form?.status === 'pending' ? 'Check your email' : 'Sign in'}</h1>
-			<SigninForm {form} />
+	<div class="flex min-h-screen flex-col items-center p-8">
+		<div class="flex flex-1 flex-col items-center justify-center">
+			<img src="/logo.png" alt="Spanner" class="h-14 w-auto dark:hidden" />
+			<img src="/logo-white.png" alt="Spanner" class="hidden h-14 w-auto dark:block" />
+		</div>
+		<div class="flex flex-[3] flex-col items-center justify-start">
+			<div class="w-full max-w-[400px] rounded-xl border bg-card p-8 shadow-sm">
+				<h1 class="h1">{form?.status === 'pending' ? 'Check your email' : 'Sign in'}</h1>
+				<SigninForm {form} />
+			</div>
 		</div>
 	</div>
 </main>
