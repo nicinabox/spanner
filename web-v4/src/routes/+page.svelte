d39563e1bd2df16c0c77c4b89c5e7d27f4f5c6ab
Nic Haynes <nic@nicinabox.com>
2026-03-22 14:02:42 -0500

    Refine sign in flow

--- web-v4/src/routes/+page.svelte
+++ web-v4/src/routes/+page.svelte
@@ -7,8 +7,8 @@
 
 <main class="container mx-auto">
 	<div class="flex h-screen flex-col items-center justify-center">
-		<div class="m-8 w-full rounded-md bg-base-200 p-8 shadow-md sm:w-[400px]">
-			<h1 class="h1">Sign In</h1>
+		<div class="m-8 w-full rounded-xl border bg-card p-8 shadow-sm sm:w-[400px]">
+			<h1 class="h1">{form?.status === 'pending' ? 'Check your email' : 'Sign in'}</h1>
 			<SigninForm {form} />
 		</div>
 	</div>
