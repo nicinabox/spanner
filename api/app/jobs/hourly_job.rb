a47e2724013025f207df7202bc1f62ea26daa4d4
Nic Haynes <nic@nicinabox.com>
2026-06-18 15:30:00 -0500

    Fix scheduler reminder timezone coverage (#8)

--- api/app/jobs/hourly_job.rb
+++ api/app/jobs/hourly_job.rb
@@ -8,38 +8,28 @@ class HourlyJob < ApplicationJob
   end
 
   def today_reminders_in_timezone
-    tz = find_midnight_timezone
-    return unless tz
+    # rubocop:disable Rails/FindEach
+    ActiveSupport::TimeZone.all.each do |tz|
+      # rubocop:enable Rails/FindEach
+      local_time = Time.zone.now.in_time_zone(tz)
+      next unless local_time.hour.zero?
 
-    send_reminders_for_timezone(tz)
-  end
+      time_zone_offset = tz.utc_offset / (60 * 60)
+      date = local_time.to_date
 
-  private
+      User.where(time_zone_offset: time_zone_offset).find_each do |user|
+        next unless user.reminder_eligible?
 
-  def find_midnight_timezone
-    ActiveSupport::TimeZone.all.find { |tz| Time.now.in_time_zone(tz).hour.zero? }
-  end
+        reminders = user.reminders.where(reminder_date: date.all_day)
+        reminders = reminders.select do |r|
+          r.vehicle.preferences.send_reminder_emails
+        end
 
-  def send_reminders_for_timezone(time_zone)
-    time_zone_offset = time_zone.utc_offset / (60 * 60)
-    date = Time.now.in_time_zone(time_zone)
-    users = User.where time_zone_offset: time_zone_offset
+        next if reminders.empty?
 
-    users.each do |user|
-      next unless user.reminder_eligible?
-
-      reminders = eligible_reminders(user, date)
-      next if reminders.empty?
-
-      RemindersMailer.reminder_today(user, reminders).deliver_now
-      user.record_reminder_sent!
-    end
-  end
-
-  def eligible_reminders(user, date)
-    reminders = user.reminders.where(reminder_date: date.all_day)
-    reminders.select do |r|
-      r.vehicle.preferences.send_reminder_emails
+        RemindersMailer.reminder_today(user, reminders).deliver_now
+        user.record_reminder_sent!
+      end
     end
   end
 end
