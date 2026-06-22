9a7b44efc753120262018290653683ba7dad7b58
Nic Haynes <nic@nicinabox.com>
2026-06-22 11:31:12 -0500

    Add vehicles stats

---
+++ web-v4/src/lib/components/common/Stat.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	interface Props {
+		title: string;
+		value: string | number | null | undefined;
+		class?: string;
+	}
+
+	let { title, value, ...props }: Props = $props();
+</script>
+
+{#if value}
+	<div class="min-w-fit py-2 {props.class}">
+		<div class="text-sm text-muted">{title}</div>
+		<div class="text-xl font-bold">{value}</div>
+	</div>
+{/if}
--- web-v4/src/routes/vehicles/+page.svelte
+++ web-v4/src/routes/vehicles/+page.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
 	import VehicleLink from '$lib/components/vehicles/VehicleLink.svelte';
+	import Stat from '$lib/components/common/Stat.svelte';
 	import { sortVehiclesBy } from '$lib/utils/sortable';
-	import type { Sortable } from '$lib/data/vehicles';
+	import { formatMileage } from '$lib/utils/vehicle';
+	import type { DistanceUnit, Sortable, Vehicle } from '$lib/data/vehicles';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
 
-	let sortable: Sortable = $derived(data.user?.preferences.vehiclesSortOrder ?? ['created_at', 'desc']);
+	let sortable: Sortable = $derived(
+		data.user?.preferences.vehiclesSortOrder ?? ['created_at', 'desc']
+	);
 
 	let active = $derived(data.vehicles.filter((v) => !v.retired));
 	let sorted = $derived(sortVehiclesBy(active, sortable));
+
+	let vehiclesByDistanceUnit = $derived(
+		Object.entries(Object.groupBy(active, (v) => v.distanceUnit)) as [DistanceUnit, Vehicle[]][]
+	);
 </script>
+
+<div class="mb-6 flex justify-start gap-16 overflow-auto pointer-coarse:no-scrollbar">
+	<Stat title="Active vehicles" value={active.length} />
+
+	{#each vehiclesByDistanceUnit as [distanceUnit, vehicles] (distanceUnit)}
+		{@const total = vehicles.reduce((acc, v) => acc + (v.milesPerYear ?? 0), 0)}
+		<Stat title={`Total ${distanceUnit} per year`} value={formatMileage(total, distanceUnit)} />
+	{/each}
+</div>
 
 <section>
 	<h1 class="text-xl mb-4">Vehicles</h1>
