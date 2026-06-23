991e8dd9bd0977cc611fffb31911d0e598506ee3
Nic Haynes <nic@nicinabox.com>
2026-06-23 16:12:16 -0500

    Add vehicle stats

--- web-v4/src/routes/vehicles/[id]/+page.svelte
+++ web-v4/src/routes/vehicles/[id]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { Button } from '$lib';
 	import PageLayout from '$lib/components/common/PageLayout.svelte';
+	import Stat from '$lib/components/common/Stat.svelte';
 	import { ArrowLeft } from 'lucide-svelte';
+	import { intlFormatDateUTC } from '$lib/utils/date';
 	import type { PageProps } from './$types';
 	import Menu from '$lib/components/common/Menu.svelte';
 	import VehicleColorIndicator from '$lib/components/vehicles/VehicleColorIndicator.svelte';
@@ -34,17 +36,24 @@
 		</div>
 	{/snippet}
 
-	<div class="max-w-xl mx-auto">
-		<div class="flex items-start justify-between mb-6">
-			<div>
-				<h1 class="text-xl">{vehicle.name}</h1>
-				<p class="text-sm text-ink-500">
-					{vehicle.estimatedMileage}
-					{vehicle.distanceUnit} &bull; {vehicle.milesPerYear}
-					{vehicle.distanceUnit}/yr
-				</p>
-			</div>
-			<Button href="/vehicles/{vehicle.id}/edit" variant="tertiary">Edit</Button>
+	<div class="max-w-6xl mx-auto">
+		<div
+			class="mb-6 flex justify-between gap-12 md:gap-16 overflow-auto pointer-coarse:no-scrollbar"
+		>
+			<Stat
+				title="Estimated mileage"
+				value={vehicle.estimatedMileage
+					? `${vehicle.estimatedMileage.toLocaleString()} ${vehicle.distanceUnit}`
+					: null}
+			/>
+			<Stat
+				title="Mileage rate"
+				value={vehicle.milesPerYear
+					? `${vehicle.milesPerYear.toLocaleString()} ${vehicle.distanceUnit}/yr`
+					: null}
+			/>
+			<Stat title="Since" value={vehicle.createdAt ? intlFormatDateUTC(vehicle.createdAt) : null} />
+			<Stat title="VIN" value={vehicle.vin} />
 		</div>
 	</div>
 </PageLayout>
