88913c35cc25c54e6cab88a451dc5cd4386e75a1
Nic Haynes <nic@nicinabox.com>
2026-06-27 14:27:27 -0500

    Replace favicon with svg

--- web-v4/src/routes/+layout.svelte
+++ web-v4/src/routes/+layout.svelte
@@ -6,38 +6,6 @@
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://rsms.me" />
-	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-
-	<link
-		rel="icon"
-		type="image/png"
-		href="/icons/favicon-32.png"
-		sizes="32x32"
-		media="(prefers-color-scheme: light)"
-	/>
-	<link
-		rel="icon"
-		type="image/png"
-		href="/icons/favicon-16.png"
-		sizes="16x16"
-		media="(prefers-color-scheme: light)"
-	/>
-
-	<link
-		rel="icon"
-		type="image/png"
-		href="/icons/favicon-white-32.png"
-		sizes="32x32"
-		media="(prefers-color-scheme: dark)"
-	/>
-	<link
-		rel="icon"
-		type="image/png"
-		href="/icons/favicon-white-16.png"
-		sizes="16x16"
-		media="(prefers-color-scheme: dark)"
-	/>
 	{import.meta.env.HEAD_INJECTIONS}
 </svelte:head>
 
