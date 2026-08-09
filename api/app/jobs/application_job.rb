c8a83c9cc73876863312b14f97ab0e3eb2ef56e9
Nic Haynes <nic@nicinabox.com>
2026-08-09 15:29:13 -0500

    Log job start/end and catch errors

--- api/app/jobs/application_job.rb
+++ api/app/jobs/application_job.rb
@@ -2,4 +2,21 @@
 
 class ApplicationJob < ActiveJob::Base
   queue_as :default
+
+  before_perform do |job|
+    Rails.logger.info(
+      "[bg] Starting #{job.class.name} (job_id=#{job.job_id}) at #{Time.now.utc.iso8601}"
+    )
+  end
+
+  after_perform do |job|
+    Rails.logger.info(
+      "[bg] Finished #{job.class.name} (job_id=#{job.job_id}) at #{Time.now.utc.iso8601}"
+    )
+  end
+
+  rescue_from(StandardError) do |exception|
+    Sentry.capture_exception(exception, extra: { active_job: self.class.name, job_id: arguments.first })
+    raise
+  end
 end
