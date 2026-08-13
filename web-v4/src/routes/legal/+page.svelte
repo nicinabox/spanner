6354d7e057f6c83de49ae23d51c40ce9e2d7cf34
Nic Haynes <nic@nicinabox.com>
2026-08-12 23:14:09 -0500

    Update legal page loading, layout

--- web-v4/src/routes/legal/+page.svelte
+++ web-v4/src/routes/legal/+page.svelte
@@ -3,18 +3,19 @@
 	import { marked } from 'marked';
 	import { pageTitle } from '$lib/utils/site';
 	import { PUBLIC_CONTACT_EMAIL } from '$app/env/public';
-	import src from './legal.md?raw';
+	import src from './page.md?raw';
+	import { PageLayout } from '$lib';
 
 	const email = PUBLIC_CONTACT_EMAIL || 'the operator of this instance';
-	const html = sanitizeHtml(
-		marked(src.replaceAll('{{CONTACT_EMAIL}}', email), { async: false }),
-	);
+	const html = sanitizeHtml(marked(src.replaceAll('{{CONTACT_EMAIL}}', email), { async: false }));
 </script>
 
 <svelte:head>
 	<title>{pageTitle('Legal')}</title>
 </svelte:head>
 
-<div class="mx-auto px-(--main-padding) py-12 prose dark:prose-invert">
-	{@html html}
-</div>
+<PageLayout>
+	<div class="mx-auto py-12 prose dark:prose-invert">
+		{@html html}
+	</div>
+</PageLayout>
